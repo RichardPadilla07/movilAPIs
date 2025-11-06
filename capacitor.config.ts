357f1cfc1b9f421712a6326697ca849351f43916
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-11-06 12:29:56 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'APIS',
+  webDir: 'www'
+};
+
+export default config;
